8dfe2212cabe9a70f9082ac190de8bc53da05a5a
AbdoLegrand <abdototo4136@gmail.com>
2024-12-05 17:24:18 +0000

    Add html file

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
       <div className="w-full max-w-2xl">
-        <Register />
+        <GameTcha />
       </div>
     </div>
   );
